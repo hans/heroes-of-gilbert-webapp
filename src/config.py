e048d7bb7b6f6659658532b49bae513d2089a0b4
Hans Engel <engel@engel.uk.to>
2013-03-09 19:37:29 -0700

    Add SITE_URL constant

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 import os
 
 NAMESPACE = 'dev'
-
 DEV = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
+
+SITE_URL = 'http://heroes-of-gilbert.appspot.com/' if not DEV else 'http://localhost:8080/'
